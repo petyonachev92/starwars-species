8fbebbf5741dc12437ff35f9f9f24029dddc5688
Petyo <pyo@abv.bg>
2021-02-09 09:56:52 +0200

    Created Species and universe

---
+++ src/app/custom/Species.js
@@ -0,0 +1,37 @@
+import config from '../../config'
+import EventEmitter from "eventemitter3";
+
+const EVENTS = {
+    SPECIES_CREATED: 'species_created'
+}
+
+
+
+export default class Species extends EventEmitter {
+    constructor() {
+        super()
+        this.name = null;
+        this.classification = null;
+    }
+
+    get events() {
+        return EVENTS;
+    }
+
+    async init(url) {
+        console.log('Species init')
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data)
+
+        this.name = data.name;
+        this.classification = data.classification;
+
+        this.emit(this.events.SPECIES_CREATED);
+
+        id++;
+        console.log(id)
+
+    }
+}
